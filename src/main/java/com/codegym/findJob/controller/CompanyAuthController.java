5319e45aa5c29742518de1584790273ff572a16a
acmong2212 <depzai02x@gmail.com>
2022-03-13 22:21:34 +0700

    send email company

--- src/main/java/com/codegym/findJob/controller/CompanyAuthController.java
+++ src/main/java/com/codegym/findJob/controller/CompanyAuthController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
